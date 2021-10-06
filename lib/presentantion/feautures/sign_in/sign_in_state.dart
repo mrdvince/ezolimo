0d941607045c141b295e44f0ecf153b666c1bb1a
Vince <vincivenv@gmail.com>
2021-10-06 06:53:06 +0300

    signin bloc

--- lib/presentantion/feautures/sign_in/sign_in_state.dart
+++ lib/presentantion/feautures/sign_in/sign_in_state.dart
@@ -1,20 +1,18 @@
 part of 'sign_in_bloc.dart';
 
 abstract class SignInState extends Equatable {
-  const SignInState();
-
   @override
   List<Object> get props => [];
 }
 
-class SignInInitial extends SignInState {}
+class SignInInitialState extends SignInState {}
 
 class SignInProcessingState extends SignInState {}
 
 class SignInErrorState extends SignInState {
-  const SignInErrorState(this.error);
-
+  SignInErrorState(this.error);
   final String error;
+
   @override
   List<Object> get props => [error];
 }
