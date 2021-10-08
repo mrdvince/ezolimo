6214ae066e5e56a711324520bb039e116af126d0
Vince <vincivenv@gmail.com>
2021-10-08 06:35:57 +0300

    fix: lint warnings

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -8,10 +8,12 @@ import 'presentation/router/app_router.dart';
 
 void main() {
   Bloc.observer = AppBlocObserver();
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/presentation/screens/home_screen/widgets/home_widget.dart
+++ lib/presentation/screens/home_screen/widgets/home_widget.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container();
