11cec809d76f62b2d21113a289bcf390741be8cb
Vince <vincivenv@gmail.com>
2021-10-08 05:08:55 +0300

    runner, presenter

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../core/constants/strings.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key, required this.title}) : super(key: key);
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
