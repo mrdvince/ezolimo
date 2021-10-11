e5787766437c7a6b3f3f07163a43741efb34e811
Vince <vincivenv@gmail.com>
2021-10-11 19:57:48 +0300

    feat: sign in bloc

--- lib/presentation/features/sign_in/sign_in_event.dart
+++ lib/presentation/features/sign_in/sign_in_event.dart
@@ -1,4 +1,20 @@
 part of 'sign_in_bloc.dart';
 
-@immutable
-abstract class SignInEvent {}
+abstract class SignInEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class SignInPressed extends SignInEvent {
+  final String email;
+  final String password;
+
+  SignInPressed({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class SignInPressedFacebook extends SignInEvent {}
+
+class SignInPressedGoogle extends SignInEvent {}
