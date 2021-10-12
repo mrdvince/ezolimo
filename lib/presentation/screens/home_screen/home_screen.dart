8ff310b59fbe66a331d628bc94d263e305e4dcd6
Vince <vincivenv@gmail.com>
2021-10-12 04:46:32 +0300

    navigate to homescreen if token is presented

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -1,29 +1,38 @@
+import '../../../domain/storage.dart';
+import '../sign_in_screen/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/constants/strings.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
-  final String title;
+  Future<String> get jwtOrEmpty async {
+    var jwt = await Storage().secureStorage.read(key: 'access_token') ?? '';
+    return jwt;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+    return MaterialApp(
+      title: 'Authentication Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              Strings.homeScreenCenterText,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      home: FutureBuilder(
+          future: jwtOrEmpty,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const CircularProgressIndicator();
+            if (snapshot.data != "") {
+              var str = snapshot.data;
+              return Scaffold(
+                  body: Center(
+                child: Text(snapshot.data as String),
+              ));
+            } else {
+              return const SignInScreen();
+            }
+          }),
     );
   }
 }
