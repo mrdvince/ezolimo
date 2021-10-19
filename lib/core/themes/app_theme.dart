98cd81e4929236d0d70ef282501775eb241b9991
Vince <vincivenv@gmail.com>
2021-10-19 05:32:54 +0300

    theme color

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class AppTheme {
   const AppTheme._();
   static final lightTheme = ThemeData(
-    backgroundColor: const Color(0xFF144016),
+    primarySwatch: Colors.green,
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 
   static final darkTheme = ThemeData(
-    backgroundColor: const Color(0xFF144016),
+    primarySwatch: Colors.green,
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
