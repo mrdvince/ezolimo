5333796b0d10ef9c5f9ef9a6feb88e068b2788cb
Vince <vincivenv@gmail.com>
2021-10-19 05:34:19 +0300

    switch to context watch

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:ezolimo/core/constants/strings.dart';
+import 'package:ezolimo/logic/predict/predict_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../logic/auth/auth_cubit.dart';
 import '../../router/app_router.dart';
@@ -9,7 +14,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    BlocProvider.of<AuthCubit>(context).isAuthenticated();
+    final cubit = context.watch<AuthCubit>();
 
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) {
@@ -20,27 +25,98 @@ class HomeScreen extends StatelessWidget {
       },
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Nebos'),
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    clearToken(context);
-                  },
-                  icon: const Icon(Icons.logout_outlined))
-            ],
-          ),
-          body: const Center(
-            child: Text('camera'),
-          ),
-        );
+            appBar: AppBar(
+              title: const Text('Nebos'),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      clearToken(cubit);
+                    },
+                    icon: const Icon(Icons.logout_outlined))
+              ],
+            ),
+            body: const Camera());
       },
     );
   }
 
   void clearToken(
-    BuildContext context,
+    cubit,
   ) {
-    BlocProvider.of<AuthCubit>(context).logOut();
+    // BlocProvider.of<AuthCubit>(context).logOut();
+    cubit.logOut();
+  }
+}
+
+class Camera extends StatefulWidget {
+  const Camera({Key? key}) : super(key: key);
+
+  @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  void _openImagePicker(BuildContext context, PredictCubit cubit) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 150.0,
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Text(
+                  Strings.pickImage,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                    onPressed: () {
+                      cubit.getImage(source: ImageSource.camera);
+                      Navigator.pop(context);
+                    },
+                    child: Text(Strings.useCamera)),
+                TextButton(
+                    onPressed: () {
+                      cubit.getImage(source: ImageSource.gallery);
+                      Navigator.pop(context);
+                    },
+                    child: Text(Strings.useGallery))
+              ],
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.watch<PredictCubit>();
+    return Column(
+      children: [
+        const SizedBox(height: 10.0),
+        cubit.state.imageFile == null
+            ? const Icon(Icons.ac_unit)
+            : Image.file(
+                File(cubit.state.imageFile.toString()),
+                fit: BoxFit.cover,
+                height: 300.0,
+                width: MediaQuery.of(context).size.width,
+              ),
+        OutlinedButton(
+          onPressed: () {
+            _openImagePicker(context, cubit);
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.camera_alt),
+              SizedBox(
+                width: 5.0,
+              ),
+              Text("Add Image")
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
