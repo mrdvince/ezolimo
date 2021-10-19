3ac963087bd8e65561da29ab45dba7ec4698516f
Vince <vincivenv@gmail.com>
2021-10-19 07:46:32 +0300

    post image

--- lib/data/http/http_client.dart
+++ lib/data/http/http_client.dart
@@ -1,3 +1,6 @@
+import 'package:http_parser/http_parser.dart';
+import 'package:mime/mime.dart';
+
 import '../../core/constants/urls.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -22,4 +25,29 @@ Future getAuthTokenReq(
   } catch (e) {
     return [];
   }
+}
+
+getObjDetPreds(token, String imageFilePath) async {
+  String url = ServerUrls.baseUrl + ServerUrls.predictUrlPrefix;
+
+  final mimeTypeData = lookupMimeType(imageFilePath)!.split('/');
+
+  final imageUploadreq =
+      http.MultipartRequest('POST', Uri.parse(url + '/?conf_thresh=0.1'));
+
+  final file = await http.MultipartFile.fromPath('file', imageFilePath,
+      contentType: MediaType(mimeTypeData[0], mimeTypeData[1]));
+
+  imageUploadreq.files.add(file);
+
+  imageUploadreq.headers['Authorization'] = 'Bearer $token';
+
+  try {
+    final streamedResponse = await imageUploadreq.send();
+    final response = await http.Response.fromStream(streamedResponse);
+    if (response.statusCode != 200 && response.statusCode != 201) {}
+
+    final responseData = json.decode(response.body);
+  } catch (e) {}
+  return '';
 }
