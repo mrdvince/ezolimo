fb50bdc43c2962e90be018aacd767a043e7621c6
Vince <vincivenv@gmail.com>
2021-10-25 02:03:09 +0300

    object repository

--- lib/data/repositories/obj_repository.dart
+++ lib/data/repositories/obj_repository.dart
@@ -5,8 +5,7 @@ import '../http/http_client.dart';
 class ObjDetectRepository {
   getObjPred(token, imageFile) async {
     final rawResponse = await getObjDetPreds(token, imageFile);
-    final Map<String, dynamic> mmap = {...rawResponse[0], ...rawResponse[1]};
-    final predictions = Predictions.fromJson(mmap);
+    final predictions = predictionsFromJson(rawResponse.toString());
     return predictions;
   }
 }
